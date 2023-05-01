d4a37f386a9522b4a289bd6e4c8387b7b40ac1e9
Gr1zzer <sjpocock8@gmail.com>
2023-05-01 09:38:53 -0600

    Here are 2 projects

---
+++ Udemy Projects/Social Media SlideShow/script.js
@@ -0,0 +1,17 @@
+const slideshow = document.querySelector(".slideshow");
+
+setInterval(() => {
+  const firstIcon = slideshow.firstElementChild;
+  firstIcon.classList.add("faded-out");
+  const thirdIcon = slideshow.children[3];
+  thirdIcon.classList.add("light");
+  thirdIcon.previousElementSibling.classList.remove("light");
+  setTimeout(() => {
+    slideshow.removeChild(firstIcon);
+    slideshow.appendChild(firstIcon);
+    setTimeout(() => {
+      firstIcon.classList.remove("faded-out");
+    }, 500);
+    firstIcon.classList.remove;
+  }, 500);
+}, 3000);
